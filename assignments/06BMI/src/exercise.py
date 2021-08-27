7f840aab7ec15d3a192cee3d5e476d4af9358d70
Javier Villarreal <a01721750@itesm.mx>
2021-08-26 19:04:10 -0500

    Trabajo acabado

--- assignments/06BMI/src/exercise.py
+++ assignments/06BMI/src/exercise.py
@@ -1,6 +1,23 @@
 def main():
-    #escribe tu código abajo de esta línea
-    
+    # Escribe el código adecuado para completar el programa
+    peso = float(input("Peso en kg: "))
+    altura = float(input("Altura en m: "))
+    if(peso <=0 ) or (altura <= 0):
+        print("Revisa tus datos, alguno de ellos es erróneo.")
+    else:
+        
+        i= peso/(altura**2)
 
-if __name__=='__main__':
+        if(i >= 0 and i <20):
+            print("PESO BAJO")
+        elif(i >= 20 and i < 25):
+            print("NORMAL")
+        elif(i >= 25 and i < 30):
+            print("SOBREPESO")
+        elif(i >= 30 and i < 40):
+            print("OBESIDAD")
+        elif(i >= 40):
+            print("OBESIDAD MORBIDA")
+
+if __name__ == '__main__':
     main()
